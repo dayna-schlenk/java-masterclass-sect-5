544ed13f7719acd86af4fbecc2ba936520d1b0fa
dayna-schlenk <dayna.schlenker@gmail.com>
2022-04-11 13:16:48 -0500

    Add shortcut to switch statement

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         // Switch statement practice
-        int switchValue = 3;
+        int switchValue = 13;
 
         switch (switchValue) {
             case 1:
@@ -10,8 +10,11 @@ public class App {
             case 2:
                 System.out.println("Value is 2");
                 break;
+            case 3:case 4:case 5: // shortcut
+                System.out.println("Value is either 3, 4, or 5");
+                break;
             default: // aka, "else"
-                System.out.println("Value is neither 1 nor 2");
+                System.out.println("Value is not 1 through 5");
                 break;
         }
     }
