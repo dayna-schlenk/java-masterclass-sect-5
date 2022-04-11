29bab5290bbbc0f5a83c1eba02199883177e5bf1
dayna-schlenk <dayna.schlenker@gmail.com>
2022-04-11 13:09:57 -0500

    Create first switch statement

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Switch statement practice
+        int switchValue = 3;
+
+        switch (switchValue) {
+            case 1:
+                System.out.println("Value is 1");
+                break;
+            case 2:
+                System.out.println("Value is 2");
+                break;
+            default: // aka, "else"
+                System.out.println("Value is neither 1 nor 2");
+                break;
+        }
     }
 }
