e8e0dbccab7cb8f5f23ac0e1f16a9e6e90670f28
dayna-schlenk <dayna.schlenker@gmail.com>
2022-04-25 10:19:17 -0500

    Write initial isPalindrome method

--- src/NumPalindrome.java
+++ src/NumPalindrome.java
@@ -1,5 +1,20 @@
 public class NumPalindrome {
     public boolean isPalindrome(int number) {
-        return // something
+        int reverse = 0;
+
+        // Run loop until number becomes 0
+        while (number != 0) {
+            // get last digit from number
+            int lastDigit = number % 10;
+            reverse++;
+            reverse *= 10;
+            reverse += lastDigit;
+
+            // remove last digit from number
+            number /= 10;
+        }
+
+        // Determine if number is equal to reverse variable & return
+        return (number == reverse);
     }
 }
